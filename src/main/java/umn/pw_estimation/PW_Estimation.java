ca78a3b3861ffaca5880d51adfb3b1cfff44aee3
Michael Levin <mlevin@umn.edu>
2026-04-23 12:23:51 -0500

    init

---
+++ src/main/java/umn/pw_estimation/PW_Estimation.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package umn.pw_estimation;
+
+import org.apache.commons.math3.linear.RealMatrix;
+/**
+ *
+ * @author mlevin
+ */
+public class PW_Estimation {
+
+    public static void main(String[] args) {
+
+        RealMatrix m;
+    }
+}
